568749e98b50e034eaf657faba0d1b3c491cf79f
liupinyi@cn.ibm.com <liupinyi@cn.ibm.com>
2020-12-24 19:31:34 +0800

    v1

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 print("Hello Welcome to Phthon world")
-
+print(1234)
---
+++ ocr-picture.py
@@ -0,0 +1,3 @@
+from kkb_ocr import ocr_word
+url = 'https://xiaoke-asset-prod.kaikeba.com/python_ai/demos/demo1.jpg'
+ocr_word(url)
